c404ee015aa53baae97a5a5e7880e13ee8c22fcb
Ankita-dev27 <ankita.srivastavaa01@gmail.com>
2025-11-11 07:47:00 -0800

    jsx component

---
+++ 02reactvite/src/one.js
@@ -0,0 +1,7 @@
+function one(){
+    return(
+        <h1>first jsx component</h1>
+    )
+}
+
+export default one;
---
+++ 02reactvite/src/one.jsx
@@ -0,0 +1,7 @@
+function One(){
+    return(
+        <h1>first jsx component</h1>
+    )
+}
+
+export default One;
